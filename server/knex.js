53378bcc18416b23c849ed5d5dd543d236a76353
tNahoko <nahoko.toyota@gmail.com>
2021-02-08 19:16:30 +0900

    connect knex and postgres

---
+++ server/knex.js
@@ -0,0 +1,16 @@
+const knex = require("knex");
+
+const config = {
+  client: "pg",
+  connection: {
+    connectionString:
+      process.env.DATABASE_URL ||
+      `postgres://${process.env.USER}:${process.env.PW}@127.0.0.1:5432/todo`,
+    ssl: process.env.HAS_SSL ? { rejectUnauthorized: false } : undefined,
+  },
+  searchPath: "public",
+};
+
+const db = knex(config);
+
+module.exports = db;
