bb558b8f55d3483730d910794d868648e9023ec3
tNahoko <nahoko.toyota@gmail.com>
2021-02-13 15:24:25 +0900

    add POST in server.js

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const path = require("path");
+
 const app = express();
 const db = require("./knex");
 const cors = require("cors");
@@ -9,16 +10,39 @@ const PORT = process.env.PORT || 9000;
 
 app.use(express.static('public'));
 app.use(express.json());
-// app.use(express.static(path.resolve(__dirname, "..", "dist")));
-// app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "..", "dist", "index.html"));
-//   });
+
 app.use(cors());
 
-app.get("/api/alldata", async (req, res) => {
+app.get("/api", async (req, res) => {
   const data = await db.select().table("list");
   res.status(200);
-  res.send(data);
+  res.json(data);
+});
+
+app.post("/api", async (req, res) => {
+  
+  if(!req.body.date || !req.body.desc) {
+    res.status(400).send({
+      message: "Both date and description are required."
+    });
+    return;
+  }
+
+  try {
+    
+    const newItem = {
+      date: req.body.date,
+      desc: req.body.desc,
+      status: "pending"
+    };
+    await db('list').insert(newItem);
+    const data = await db.select().table("list");
+    console.log(data)
+    res.status(200);
+    res.json(data);
+  } catch (error) {
+    res.sendStatus(400);
+  }
 });
 
 (async () => {
